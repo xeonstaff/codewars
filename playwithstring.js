e17e220177d46773fc3d21f5cf2893e8e53beed8
Joel <joelmounts@gmail.com>
2022-12-29 10:06:57 -0500

    Create playwithstring.js

---
+++ playwithstring.js
@@ -0,0 +1,52 @@
+//for input strings a and b:
+//For every character in string a swap the casing of every occurrence of the same character in string b. 
+//Then do the same casing swap with the inputs reversed.
+
+function workOnStrings(a, b) {
+  let a_array = Array.from(a);
+  let a_lower = a_array.map((x) => x.toLowerCase());
+  let b_array = Array.from(b);
+  let b_lower = b_array.map((x) => x.toLowerCase());
+
+  function swapCase(char) {
+    let smet = "";
+    if (char == char.toUpperCase()) {
+      smet = char.toLowerCase();
+    }
+    if (char == char.toLowerCase()) {
+      smet = char.toUpperCase();
+    }
+    return smet;
+  }
+
+  a_array = a_array.map((x) => {
+    let b_count = 0;
+    if (b_lower.includes(x.toLowerCase())) {
+      for (i = 0; i < b_lower.length; i++) {
+        if (b_lower[i] == x.toLowerCase()) {
+          b_count += 1;
+        }
+      }
+    }
+    if (b_count % 2 == 1) {
+      return swapCase(x);
+    }
+    return x;
+  });
+
+  b_array = b_array.map((x) => {
+    let a_count = 0;
+    if (a_lower.includes(x.toLowerCase())) {
+      for (i = 0; i < a_lower.length; i++) {
+        if (a_lower[i] == x.toLowerCase()) {
+          a_count += 1;
+        }
+      }
+    }
+    if (a_count % 2 == 1) {
+      return swapCase(x);
+    }
+    return x;
+  });
+
+  return a_array.join("") + b_array.join("");
