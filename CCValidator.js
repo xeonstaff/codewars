d28319ccea11f6583477d341a402d7e707e71bc4
Joel <joelmounts@gmail.com>
2023-03-13 11:45:22 -0400

    Update CCValidator.js

--- CCValidator.js
+++ CCValidator.js
@@ -1,17 +1,16 @@
 //validates credit card numbers through the Luhn algorithm
 
 function validate(n){
-  if(!n) return false
-  if(n==91) return true
-  let numarray = Array.from(String(n),Number)
-  const firstindex = numarray.length %2 == 0 ? 0 : 1
-  for(i=firstindex; i<numarray.length; i+=2){
+  let numarray = Array.from(String(n),Number).reverse()
+  for(i=1; i<numarray.length; i+=2){
     let newnum = numarray[i]*2
     let newnumarray = newnum.toString().split('')
+    console.log(newnumarray)
 
-    if(newnumarray.length==2) newnum = newnumarray[0] + newnumarray[1]
+    //summing any 2-digit numbers
+    if(newnumarray.length==2) newnum = parseInt(newnumarray[0]) + parseInt(newnumarray[1])
     numarray[i] = newnum
     } 
-
-  return numarray.reduce((a,b)=>a+b,0)%10 == 0
+  
+  return numarray.reduce((a,b)=>parseInt(a)+parseInt(b),0)%10 == 0
 }
