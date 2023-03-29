aaceeb4fcf0500bc906f49cce581cbe672f1c750
Joel <joelmounts@gmail.com>
2023-03-29 10:28:17 -0400

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,14 +1,25 @@
-#two methods to output the factorial n! of a number n
+#3 methods to output the factorial n! of a number n
+
+from math import factorial
+
+
 
 import functools
 
-def funfactorial(n):
+def factorial1(n):
     return functools.reduce(lambda x,y:x*y, list(range(1,n+1)))
 
 
-def factorial(n):
+
+def factorial2(n):
     rangelist = list(range(1,n+1))
     factorial = 1
     for x in rangelist:
         factorial *= x
     return factorial
+
+
+
+factorial(10)
+factorial1(10)
+factorial2(10)
