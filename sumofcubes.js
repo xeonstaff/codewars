499328ca03975c523e98a3a7c0a5c10ae0294731
Joel <joelmounts@gmail.com>
2023-11-01 10:42:36 -0400

    Update sumofcubes.js

--- sumofcubes.js
+++ sumofcubes.js
@@ -3,15 +3,15 @@
 function sumCubes(n){
    const cubearray = Array.from({length: n}, (_,i)=>i+1)
    let cubesum = 0
-   cubearray.forEach(num => cubesum+=num**2)
+   cubearray.forEach(num => cubesum+=num**3)
    return cubesum
 }
 
 //test 1 - should return: 1
 console.log(sumCubes(1))
 
-//test 2 - should return: 55
+//test 2 - should return: 225
 console.log(sumCubes(5))
 
-//test 3 - should return: 285
+//test 3 - should return: 2025
 console.log(sumCubes(9))
